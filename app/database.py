9e7ca79d4d7cbf2e36515c7c2e76696c76f748ff
pamelacarsonbri <pamelacarsonbri@gmail.com>
2026-04-18 23:39:54 +0100

    Add database connection with SQLAlchemy and MySQL

---
+++ app/database.py
@@ -0,0 +1,27 @@
+Database.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set in the .env file")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    """Dependency that provides a database session per request."""
+    db = SessionLocal()
+    try:
+          yield db
+    finally:
+        db.close()
+
